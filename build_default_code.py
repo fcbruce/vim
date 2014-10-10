56c89fe71160e3992a74cc4176593d342ce6bca9
fcbruce <fcbruce8964@gmail.com>
2014-10-10 23:25:16 +0800

    Update build_default_code.py

--- build_default_code.py
+++ build_default_code.py
@@ -12,6 +12,35 @@
   you just add your code in the text file which named 'default_code'
   and the vim source code will be built in the file 'used_in_vimrc'
 
+  for example,you need add the following code in your cpp files:
+
+    #include<bits/stdc++.h>
+
+    using namespace std;
+
+    int main()
+    {
+
+
+      return 0;
+    }
+
+  you could paste above code in the text file 'default_code', then run 
+  this scrpit, you will get the following code in the file 'used_in_vimrc':
+
+    call setline(1,"#include<bits/stdc++.h>")
+    call append(line("."),"")
+    call append(line(".")+1,"using namespace std;")
+    call append(line(".")+2,"")
+    call append(line(".")+3,"int main()")
+    call append(line(".")+4,"{")
+    call append(line(".")+5,"")
+    call append(line(".")+6,"")
+    call append(line(".")+7,"  return 0;")
+    call append(line(".")+8,"}")
+
+  and you could paste above code in file '.vimrc'
+
 """
 
 file_read=open('default_code','r')
